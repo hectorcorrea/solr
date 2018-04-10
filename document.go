b6a38713730635c86174949eccc20dbcc7f2d666
Hector Correa <hector@hectorcorrea.com>
2018-04-09 21:42:48 -0400

    Better handle of Value() and Values()

--- document.go
+++ document.go
@@ -38,39 +38,32 @@ func NewDocumentFromSolrResponse(raw responseRaw) []Document {
 	return docs
 }
 
-// Returns the value in a single-value field
+// Returns the value in a field. Concatenates multi-value
+// fields into a single string.
 func (d Document) Value(fieldName string) string {
-	// Casting to string would have been cleaner but it _only_ works for strings.
-	// Casting to interface{} allows us to fetch the value even if it is not
-	// a string (e.g a float). The downside is that fmt.Sprintf() returns a
-	// funny value for non-strings, but at least we fetch the value.
-	value, ok := d.Data[fieldName].(interface{})
-	if ok {
-		return fmt.Sprintf("%s", value)
-	}
-	return ""
+	values := d.Values(fieldName)
+	return strings.Join(values, " ")
 }
 
 // Returns all the values in a multi-value field
+// (mimics an array of one if the field is single value)
 func (d Document) Values(fieldName string) []string {
 	var values []string
 	dynValue := reflect.ValueOf(d.Data[fieldName])
-	if dynValue.Kind() == reflect.Slice {
+	kind := dynValue.Kind()
+	if kind == reflect.Invalid {
+		return values
+	}
+	if kind == reflect.Slice {
 		for i := 0; i < dynValue.Len(); i++ {
 			strValue := fmt.Sprintf("%s", dynValue.Index(i))
 			values = append(values, strValue)
 		}
+		return values
 	}
+	strValue := fmt.Sprintf("%s", dynValue)
+	values = append(values, strValue)
 	return values
-}
-
-// Returns the first value in a multi-value field
-func (d Document) FirstValue(fieldName string) string {
-	values := d.Values(fieldName)
-	if len(values) == 0 {
-		return ""
-	}
-	return values[0]
 }
 
 func (d Document) ValueFloat(fieldName string) float64 {
