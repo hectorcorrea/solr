512feeecdccab6fc095675a2e3a8e946ba02b837
Hector Correa <hector@hectorcorrea.com>
2018-04-14 21:21:47 -0400

    Removed redundant tests

--- solr_data_test.go
+++ solr_data_test.go
@@ -1,26 +1,26 @@
 package solr
 
-// These tests depends an instance of Solr
-// running at http://localhost:8983/solr/bibdata
-
 import (
 	"net/url"
 	"testing"
 )
+
+// These tests depends an instance of Solr
+// running at http://localhost:8983/solr/bibdata
+const solrCoreUrl = "http://localhost:8983/solr/bibdata"
 
 func TestGetData(t *testing.T) {
 	q := "id:00009565"
 	fl := []string{}
 	options := map[string]string{}
 	params := NewGetParams(q, fl, options)
-	solr := New("http://localhost:8983/solr/bibdata", false)
+	solr := New(solrCoreUrl, false)
 	_, err := solr.Get(params)
 	if err != nil {
 		t.Errorf("Get error: %s", err)
 	}
 }
 
-// Depends on Solr running at http://localhost:8983/solr/bibdata
 func TestHighlightsData(t *testing.T) {
 	qs := url.Values{
 		"q": []string{"george"},
@@ -34,7 +34,7 @@ func TestHighlightsData(t *testing.T) {
 	params := NewSearchParamsFromQs(qs, options, facets)
 	params.Fl = []string{"id", "authorsAll", "title"}
 
-	solr := New("http://localhost:8983/solr/bibdata", false)
+	solr := New(solrCoreUrl, false)
 	results, err := solr.Search(params)
 	if err != nil {
 		t.Errorf("Get error: %s", err)
@@ -51,44 +51,22 @@ func TestHighlightsData(t *testing.T) {
 }
 
 func TestPostData(t *testing.T) {
-	doc1 := Document{
+	doc := Document{
 		Data: map[string]interface{}{
 			"id":           "123",
 			"author":       "ada lovelace",
-			"authorsOther": []string{"a", "b", "c"},
+			"authorsOther": []string{"a", "b"},
 		},
 	}
-
-	doc2 := Document{
-		Data: map[string]interface{}{
-			"id":           "456",
-			"author":       "grace hopper",
-			"authorsOther": []string{"x", "y"},
-		},
-	}
-
-	solr := New("http://localhost:8983/solr/bibdata", false)
-	docs := []Document{doc1, doc2}
-	err := solr.PostDocs(docs)
-	if err != nil {
-		t.Errorf("PostDocs error: %s", err)
-	}
-
-	doc3 := Document{
-		Data: map[string]interface{}{
-			"id":           "789",
-			"author":       "karla loya",
-			"authorsOther": []string{"k", "l"},
-		},
-	}
-	err = solr.PostDoc(doc3)
+	solr := New(solrCoreUrl, false)
+	err := solr.PostDoc(doc)
 	if err != nil {
 		t.Errorf("PostDoc error: %s", err)
 	}
 
 	data := map[string]interface{}{
-		"id":     "000",
-		"author": "mac loya",
+		"id":     "456",
+		"author": "grace hopper",
 	}
 	err = solr.PostOne(data)
 	if err != nil {
